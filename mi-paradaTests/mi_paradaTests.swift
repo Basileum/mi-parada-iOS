da6fb87225aeea3d4e53b962858517fc7c2268f3
BA <basile.audard@outlook.com>
2025-07-03 19:19:43 +0200

    Initial Commit

---
+++ mi-paradaTests/mi_paradaTests.swift
@@ -0,0 +1,16 @@
+//
+//  mi_paradaTests.swift
+//  mi-paradaTests
+//
+//  Created by Basile on 03/07/2025.
+//
+
+import Testing
+
+struct mi_paradaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
